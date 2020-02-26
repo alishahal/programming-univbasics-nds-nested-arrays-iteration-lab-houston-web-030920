ed723ff0ef06467bba2ae95161e1ab549c3e4bf1
Karim Macknojia <alishahal@msn.com>
2020-02-26 19:08:58 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -27,6 +27,21 @@ end
   # Produce a new Array that contains the larger number of each of the pairs
   # that are in the inner Arrays
   
+    newArray = []
+  while row < src.count do 
+    col = 1 
+    while col < src[row].count do
+      if src[row][col-1] < src[row][col]
+        newArray << src[row][col]
+      else
+        newArray << src[row][col-1]
+      end
+      col +=1
+    end
+    row +=1
+  end
+  newArray
+  
  
   
   
